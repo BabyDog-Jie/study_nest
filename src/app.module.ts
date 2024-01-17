4dae7eab37c34655efa3cdbd51aef4694b6d5c4c
zhangjunjie <jjzhang31@iflytek.com>
2024-01-17 21:58:54 +0800

    refactor: add todo content

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// TODO 自动加载目标目录下的模块文件（controller, service, database）
 @Module({
   imports: [],
   controllers: [AppController],
